12962e2a86fb3c979cec796750b5b14aa45fc9ef
jignesh119 <jignesh3503@gmail.com>
2024-08-22 20:56:39 +0530

    setup merchant app, fix:workspace package import dependency

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <div className="bg-red-50">merchant there</div>;
+}
--- apps/user-app/app/balance/page.tsx
+++ apps/user-app/app/balance/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+//NOTE:
+//@ts-ignore
 import { useBalance } from "@repo/store/useBalance";
 
 export default function () {
   const balance = useBalance();
-  return <div>hi there {balance}</div>;
+  return <div>your balance: 0$ {balance}</div>;
 }
